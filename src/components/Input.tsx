3610f3eb97a2ef53be871e81961d3e6cdb9957ae
KANGJIYEON2 <hihello516@naver.com>
2024-08-03 16:47:05 +0900

    이미지 업로드 및 app 정리

---
+++ src/components/Input.tsx
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export class Input extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Input</Text>
+      </View>
+    );
+  }
+}
+
+export default Input;
--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
+import LoginPage from './LoginPage';
 
 const StatisticsPage: React.FC = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>StatisticsPage</Text>
+    <View>
+      <LoginPage />
     </View>
   );
 };
