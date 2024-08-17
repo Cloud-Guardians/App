10d90462d4ebdec15e2b0df3148a766ce0a30aca
JeHeeYu <sees1234558@gmail.com>
2024-08-17 18:50:18 +0900

    feat: PostingDetailPage 댓글 버튼 추가

--- src/pages/Community/CommunityPage.tsx
+++ src/pages/Community/CommunityPage.tsx
@@ -5,7 +5,7 @@ import Posting from './Posting/Posting';
 import { Post } from '../../types/community';
 
 const dummyPosts: Post[] = [
-  { id: 1, title: '술 마시면 개', content: '오늘은 나와의 약속을 거슬렀다 이 바', writer: '김삼배 01:37 조회 55', favoriteCount: 3, commentCount: 3, isBest: true, },
+  { id: 1, title: '술 마시면 개', content: '오dasdasdadsadasdasdadgsauydgasyudgzzzzzzzzzzzxzxzxzxzxzxzxzxzxzxzxzxzxzxzxzxzxz asyudgauydgasyudsagdyuagdasuydgaysu늘은1\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은\n오늘은22\n 나와의 약속을 거슬렀다 이 바', writer: '김삼배 01:37 조회 55', favoriteCount: 3, commentCount: 3, isBest: true, },
   { id: 2, title: '테슬라가 싫다', content: '주식 생각에 점심을 못 먹었다 하필...', writer: '테슬라짱 00:53 조회 76', favoriteCount: 5, commentCount: 1, isBest: false },
   { id: 3, title: '술 마시면 개', content: '오늘은 나와의 약속을 거슬렀다 이 바', writer: '김삼배 01:37 조회 55', favoriteCount: 3, commentCount: 3, isBest: true, },
   { id: 4, title: '테슬라가 싫다', content: '주식 생각에 점심을 못 먹었다 하필...', writer: '테슬라짱 00:53 조회 76', favoriteCount: 5, commentCount: 1, isBest: false },
